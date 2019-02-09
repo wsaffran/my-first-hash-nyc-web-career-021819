cd58b78b101f035c9fb56290b29a181a3cc08baa
Will Saffran <willh.saffran@gmail.com>
2019-02-09 03:40:26 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -36,9 +36,9 @@ def adding
 
 
   # add 4 gun powder to the shipping_manifest hash below
-
+  shipping_manifest["gun powder"] = 4
 
 
   # return the shipping_manifest hash below
-
+  shipping_manifest
 end
